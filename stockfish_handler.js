f103496d85220e8dc8d81c4cce8756ea4df73200
beroa <alanberotti@gmail.com>
2023-11-07 23:37:34 -0500

    cleanup + recognize setoption

--- stockfish_handler.js
+++ stockfish_handler.js
@@ -35,7 +35,8 @@ class StockfishHandler {
 
       if (command.startsWith('position')) {
         resolve('position set')
-      }
+      } else if (command.startsWith('setoption'))
+        resolve('option set')
     });
   }
 
@@ -47,14 +48,6 @@ class StockfishHandler {
     } else {
       return buffer.includes('\n')
     }
-
-    // Implement logic to determine if the end of the response has been reached
-    // This will vary depending on the command sent and the expected response
-    // For example:
-    // if (command.startsWith('go')) {
-    //   return buffer.includes('bestmove');
-    // }
-    // Add other conditions as necessary based on the commands you are sending
   }
 
 
@@ -66,57 +59,3 @@ class StockfishHandler {
 }
 
 module.exports = StockfishHandler;
-
-// const { spawn } = require('child_process');
-
-// class StockfishHandler {
-//   constructor() {
-//     this.stockfish = spawn('stockfish');
-//     this.buffer = '';
-//     this.setupEventHandlers();
-//   }
-
-//   setupEventHandlers() {
-//     this.stockfish.stdout.on('data', (data) => {
-//       this.buffer += data.toString();
-//       // Trigger events or resolve promises here when complete lines are received.
-
-//       console.log('this.buffer', this.buffer)
-//       // console.log(`Stockfish response: ${data}`)
-//     });
-
-//     this.stockfish.stderr.on('data', (data) => {
-//       console.error(`Error: ${data}`);
-//     });
-
-//     this.stockfish.on('close', (code) => {
-//       console.log(`Stockfish process exited with code ${code}`);
-//     });
-//   }
-
-//   sendCommand(command) {
-//     console.log(`Sending command to Stockfish: ${command}`);
-//     this.stockfish.stdin.write(`${command}\n`);
-//     return new Promise((resolve) => {
-//       const listener = (data) => {
-//         console.log('data here', data)
-//         if (data.toString().includes('ponder')) {
-//           // Remove the listener once we have a complete line
-//           this.stockfish.stdout.removeListener('data', listener);
-//           console.log('resolving with', this.buffer)
-//           resolve(this.buffer);
-//           this.buffer = ''; // Clear the buffer after resolving
-//         }
-//       };
-//       this.stockfish.stdout.on('data', listener);
-//     });
-//   }
-
-//   init() {
-//     this.sendCommand('uci');
-//     this.sendCommand('isready');
-//     this.sendCommand('ucinewgame');
-//   }
-// }
-
-// module.exports = StockfishHandler;
--- webserver.js
+++ webserver.js
@@ -25,7 +25,7 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
